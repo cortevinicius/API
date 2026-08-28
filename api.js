ccf903df3ffaac28eb8c8584f6c86bd03ace365c
Vinícius Côrte <cortevinicius1@gmail.com>
2026-08-27 22:06:25 -0300

    feat: cria API de data e hora

---
+++ api.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("API de Data e Hora funcionando!");
+});
+
+app.get("/data-hora", (req, res) => {
+    const agora = new Date();
+
+    res.json({
+        data: agora.toLocaleDateString("pt-BR"),
+        hora: agora.toLocaleTimeString("pt-BR")
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
